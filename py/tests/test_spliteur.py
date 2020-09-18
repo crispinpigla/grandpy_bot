fc871846621e1db4a33e20d02b8713845b716de9
Crispin Pigla <crispinpigla@yahoo.fr>
2020-09-18 16:35:17 +0200

    probleme import tests

--- py/tests/test_spliteur.py
+++ py/tests/test_spliteur.py
@@ -1,14 +1,20 @@
 """Test"""
 
-from .. import spliteur
 
-def test_spliteur():
-	"""Test"""
-	input_value = "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
-	spliteur0 = spliteur.Spliteur()
-	spliteur0.split_and_clean(input_value)
-	assert spliteur0.str_replaced_ponctuation == "Salut GrandPy   Est ce que tu connais l adresse d OpenClassrooms  "
-	assert spliteur0.split_byspace == ['Salut', '', 'GrandPy', '', '', '', 'Est', 'ce', 'que', 'tu',
-	'connais', 'l', 'adresse','d', 'OpenClassrooms', '', ''  ]
-	assert spliteur0.list_clean == ['Salut', 'GrandPy', 'Est', 'ce', 'que', 'tu', 'connais', 'l', 'adresse','d', 'OpenClassrooms']
+from parser import Parser
+
+
+class TestParser:
+	"""docstring for TestParser"""
+
+	def test_parser(self):
+		"""Test"""
+
+		input_value = "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
+		parser0 = Parser()
+		parser0.handle_message("Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?")
+		assert parser0.str_replaced_ponctuation == "Salut GrandPy   Est ce que tu connais l adresse d OpenClassrooms  "
+		assert parser0.split_byspace == ['Salut', '', 'GrandPy', '', '', '', 'Est', 'ce', 'que', 'tu',
+		'connais', 'l', 'adresse','d', 'OpenClassrooms', '', ''  ]
+		assert parser0.list_clean == ['Salut', 'GrandPy', 'Est', 'ce', 'que', 'tu', 'connais', 'l', 'adresse','d', 'OpenClassrooms']
 
